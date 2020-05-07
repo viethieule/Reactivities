b50fb4fceabb932be531d049ab1c924d463fd601
Hieu Le <viethieule@gmail.com>
2020-05-07 18:26:41 +0700

    End of section 17

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -34,7 +34,7 @@ const NavBar = () => {
               <Dropdown.Menu>
                 <Dropdown.Item
                   as={Link}
-                  to={`/profile/username`}
+                  to={`/profile/${user.username}`}
                   text="My profile"
                   icon="user"
                 />
